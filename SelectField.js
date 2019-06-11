afd724cafdd81987fbf0c06bdcade3db5df57158
Brandon <blshort43@users.noreply.github.com>
2019-06-11 13:09:59 -0500

    Updated SelectField
    SelectField now takes the label given and uses it for the placeholder by default. The default placeholder is removed when the field is focused and returns if the user doesn't select an option/leaves the field.

--- SelectField.js
+++ SelectField.js
@@ -5,7 +5,6 @@
  *
  */
 // NOTE Use 'required' on SelectField and <option value="" hidden>Your default value here</option> to display a faded default value
-
 // NOTE Example:
 // <SelectField
 // required
@@ -23,91 +22,112 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { Box, Card } from 'rebass';
+import { Card } from 'rebass';
 
-const RebassTextfield = styled(Card)`
+const StyledSelectField = styled.select`
   outline: none;
-  border-radius: 4px;
   font-family: inherit;
-  height: 56px;
-  padding-left: 12px;
-  min-width: 180px;
-  white-space: nowrap;
-
+  padding: ${props => (props.padding ? props.padding : '12px')};
+  transition: all 0.25s linear;
+  margin: 0;
+  box-sizing: border-box;
+  border: ${props => (props.border ? props.border : '1px solid #909090')};
+  border-radius: ${props => (props.borderRadius ? props.borderRadius : '6px')};
+  width: 100%;
+  max-width: ${props => (props.maxWidth ? props.maxWidth : '300px')};
+  min-width: 144px;
   :invalid {
     color: gray;
   }
   :hover {
     border: solid 1px black;
     color: black;
+    ::placeholder {
+      color: black;
+      opacity: 1;
+    }
   }
   :focus {
-    border: 1.5px solid #3a71ff;
+    outline-offset: 0;
+    border: solid 1px #2e66ff;
     color: black;
+    ::placeholder {
+      opacity: 0;
+    }
   }
 `;
 
 class SelectField extends React.PureComponent {
   state = {
-    opacity: '0',
+    focused: false,
+    showPlaceholder: true,
   };
 
-  componentDidMount() {
-    if (this.props.value === '' || this.props.value === null) {
-      this.setState({ opacity: '0' });
+  handleHover = () => {
+    if (this.props.value) {
+      this.setState({ focused: true });
     }
-  }
-
-  showLabel = () => {
-    this.setState({ opacity: '1' });
   };
 
-  hideLabel = e => {
-    const { value } = e.target;
+  handleFocus = () => {
+    this.setState({ focused: true, showPlaceholder: false });
+  };
 
-    if (value === '') {
-      this.setState({ opacity: '0' });
+  handleBlur = () => {
+    if (this.props.value) {
+      this.setState({ focused: true });
+    } else {
+      this.setState({ focused: false, showPlaceholder: true });
     }
   };
 
   render() {
     const { ...props } = this.props;
+
     return (
-      <Box
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          width: 'fit-content',
-        }}
-        margin={this.props.margin}
-        marginTop={this.props.marginTop}
-        marginLeft={this.props.marginLeft}
-        marginRight={this.props.marginRight}
+      <Card
+        m={props.margin || props.m || 0}
+        mt={props.mt || 3}
+        p={0}
+        bg={props.bg}
+        borderRadius={props.borderRadius || '6px'}
+        width={props.width}
       >
         <legend
           style={{
             fontSize: '12px',
-            opacity: `${this.state.opacity}`,
-            transition: 'opacity .25s ease-in-out',
-            marginTop: '-18px',
+            opacity: `${this.state.focused ? 1 : 0}`,
+            transition: 'all .25s ease-in-out',
+            position: 'absolute',
+            boxSizing: 'border-box',
+            transform: `${
+              this.state.focused ? 'translate(0, -18px)' : 'translate(10px, 0)'
+            }`,
           }}
         >
           {props.label}
         </legend>
-        <RebassTextfield
+        <StyledSelectField
           {...props}
-          as="select"
-          margin="0"
-          marginTop="0"
-          marginLeft="0"
-          marginRight="0"
-          border={!this.props.border ? '1px solid #909090' : props.border}
-          onFocus={this.showLabel}
-          onBlur={this.hideLabel}
+          value={props.value}
+          name={props.name}
+          onMouseOver={this.handleHover}
+          onFocus={this.handleFocus}
+          onBlur={this.handleBlur}
         >
+          {this.state.showPlaceholder ? (
+            <option value="" hidden>
+              {props.label}
+            </option>
+          ) : (
+            <option label="" hidden>
+              {props.label}
+            </option>
+          )}
+
           {props.children}
-        </RebassTextfield>
-      </Box>
+        </StyledSelectField>
+      </Card>
     );
   }
 }
