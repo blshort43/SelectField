f96533c0f001532389af5ad4439ba53bc8827d3c
Brandon <blshort43@users.noreply.github.com>
2019-08-13 14:48:29 -0500

    Reverted changes, moving changes to SelectFieldV3
    Reverted changes, moving changes to SelectFieldV3

--- SelectField.js
+++ SelectField.js
@@ -34,168 +34,110 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { Card } from 'rebass';
 
-const StyledCard = styled(Card)`
-   width: ${props => (props.width ? props.width : '200px')};
-   height: ${props => (props.height ? props.height : '45px')};
-   border-radius: ${props => (props.borderRadius ? props.borderRadius : '6px')};
-   margin-top: ${props =>
-      props.marginTop || props.mt ? props.marginTop || props.mt : '17px'};
-`;
-
 const StyledSelectField = styled.select`
-   color: ${props => (props.color ? props.color : '#000000')};
-   outline: none;
-   font-family: inherit;
-   padding: ${props =>
-      props.padding || props.p ? props.padding || props.p : '12px'};
-   transition: all 0.25s linear;
-   box-sizing: border-box;
-   border: ${props => {
-      if (props.error) {
-         return props.errorBorder || '1px solid red';
-      }
-      return props.border || '1px solid #909090';
-   }};
-   ::-webkit-datetime-edit {
-      opacity: ${props => props.value === '' && '0.6'};
-   }
-   background: ${props => props.background || props.bg || 'none'};
-   border-radius: ${props => (props.borderRadius ? props.borderRadius : '6px')};
-   width: 100%;
-   height: 100%;
-   :invalid {
-      color: gray;
-   }
-   ::placeholder {
-      color: ${props => (props.color ? props.color : '#000000')};
-   }
-
-   :hover {
-      ::-webkit-datetime-edit {
-         opacity: 1;
-      }
-      color: ${props =>
-         props.colorHover ? props.colorHover : props.color || '#000000'};
-      border: ${props => {
-         if (props.error) {
-            return props.errorBorder || '1px solid red';
-         }
-         return props.borderHover
-            ? props.borderHover
-            : props.border || '1px solid #000000';
-      }};
-      background: ${props =>
-         props.backgroundHover
-            ? props.backgroundHover
-            : props.background || props.bg || 'none'};
-      cursor: text;
-      ::placeholder {
-         opacity: 1;
-      }
-   }
-   :focus {
-      outline-offset: 0;
-      color: ${props =>
-         props.colorFocus ? props.colorFocus : props.color || '#000000'};
-      border: ${props => {
-         if (props.error) {
-            return props.errorBorder || '1px solid red';
-         }
-         return props.borderFocus
-            ? props.borderFocus
-            : props.border || '1px solid #2e66ff';
-      }};
-      background: ${props =>
-         props.backgroundFocus
-            ? props.backgroundFocus
-            : props.background || props.bg || 'none'};
-      ::placeholder {
-         opacity: 0;
-      }
-   }
-`;
-
-const StyledLegend = styled.legend`
-   color: ${props => (props.labelColor ? props.labelColor : '#000000')};
-   outline: none;
-   pointer-events: none;
-   font-size: 12px;
-   transition: all 0.25s ease-in-out;
-   position: absolute;
-   box-sizing: border-box;
+  outline: none;
+  font-family: inherit;
+  padding: ${props => (props.padding ? props.padding : '12px')};
+  transition: all 0.25s linear;
+  margin: 0;
+  box-sizing: border-box;
+  border: ${props => (props.border ? props.border : '1px solid #909090')};
+  border-radius: ${props => (props.borderRadius ? props.borderRadius : '6px')};
+  width: 100%;
+  max-width: ${props => (props.maxWidth ? props.maxWidth : '300px')};
+  min-width: 144px;
+  :invalid {
+    color: gray;
+  }
+  :hover {
+    border: solid 1px black;
+    color: black;
+    ::placeholder {
+      color: black;
+      opacity: 1;
+    }
+  }
+  :focus {
+    outline-offset: 0;
+    border: solid 1px #2e66ff;
+    color: black;
+    ::placeholder {
+      opacity: 0;
+    }
+  }
 `;
 
 class SelectField extends React.PureComponent {
-   state = {
-      focused: false,
-      showPlaceholder: true
-   };
+  state = {
+    focused: false,
+    showPlaceholder: true,
+  };
 
-   handleFocus = () => {
-      this.setState({ focused: true, showPlaceholder: false });
-   };
+  handleHover = () => {
+    if (this.props.value) {
+      this.setState({ focused: true });
+    }
+  };
 
-   handleBlur = () => {
-      if (this.props.value) {
-         this.setState({ showPlaceholder: true });
-      } else {
-         this.setState({ focused: false, showPlaceholder: true });
-      }
-   };
+  handleFocus = () => {
+    this.setState({ focused: true, showPlaceholder: false });
+  };
 
-   render() {
-      const {
-         type,
-         name,
-         value,
-         label,
-         labelColor,
-         error,
-         errorText,
-         placeholder,
-         required,
-         children,
-         ...rest
-      } = this.props;
+  handleBlur = () => {
+    if (this.props.value) {
+      this.setState({ focused: true });
+    } else {
+      this.setState({ focused: false, showPlaceholder: true });
+    }
+  };
 
-      return (
-         <StyledCard
-            {...rest}
-            onFocus={this.handleFocus}
-            onBlur={this.handleBlur}
-         >
-            <StyledLegend
-               labelColor={labelColor}
-               style={{
-                  opacity: `${this.state.focused ? 1 : 0}`,
-                  transform: `${
-                     this.state.focused
-                        ? 'translate(0, -18px)'
-                        : 'translate(10px, 0)'
-                  }`
-               }}
-            >
-               {label}
-            </StyledLegend>
-            <StyledSelectField
-               // ref="yearSelect"
-               error={error}
-               required={required}
-               placeholder={placeholder || ''}
-               value={value || ''}
-               type={type || 'text'}
-               name={name}
-            >
-               {this.state.showPlaceholder && <option value="">{label}</option>}
-               {children}
-            </StyledSelectField>
-         </StyledCard>
-      );
-   }
+  render() {
+    const { ...props } = this.props;
+
+    return (
+      <Card
+        m={props.margin || props.m || 0}
+        mt={props.mt || 3}
+        p={0}
+        bg={props.bg}
+        borderRadius={props.borderRadius || '6px'}
+        width={props.width}
+      >
+        <legend
+          style={{
+            fontSize: '12px',
+            opacity: `${this.state.focused ? 1 : 0}`,
+            transition: 'all .25s ease-in-out',
+            position: 'absolute',
+            boxSizing: 'border-box',
+            transform: `${
+              this.state.focused ? 'translate(0, -18px)' : 'translate(10px, 0)'
+            }`,
+          }}
+        >
+          {props.label}
+        </legend>
+        <StyledSelectField
+          {...props}
+          value={props.value}
+          name={props.name}
+          onMouseOver={this.handleHover}
+          onFocus={this.handleFocus}
+          onBlur={this.handleBlur}
+        >
+          {this.state.showPlaceholder ? (
+            <option value="">{props.label}</option>
+          ) : null}
+
+          {props.children}
+        </StyledSelectField>
+      </Card>
+    );
+  }
 }
 
 SelectField.propTypes = {
-   children: PropTypes.node.isRequired
+  children: PropTypes.node.isRequired,
 };
 
 export default SelectField;
